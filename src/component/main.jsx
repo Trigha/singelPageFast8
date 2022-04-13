ca687b780703fb3f334d908013bf1478a44fc27a
Trigha <adhindyatrigha@gmail.com>
2022-04-13 20:53:37 +0700

    update main content desktop look

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -106,7 +106,7 @@ function Main() {
           {/* Heading main content */}
           <div class="w-64 md:w-full  drop-shadow-lg rounded-xl mb-6 pt-10">
             <div class="bg-white border-transparent border-4 md:w-full shadow-sm">
-              <div class="p-2 px-5">
+              <div class="p-2 md:px-5">
                 <div class="flex flex-col">
                   <h1 class="flex items-start uppercase  text-primary font-bold text-2xl">
                     personnel list
@@ -114,13 +114,13 @@ function Main() {
                   <h2 class="flex items-start normal-case text-secondary text-lg mb-2">
                     List of all personnels
                   </h2>
-                  <div class="flex flex-col md:flex-row md:absolute md:bottom-2 md:left-[58%] md:space-x-7">
+                  <div class="flex flex-col md:flex-row md:absolute md:bottom-2 md:left-[58%] space-x-5">
                     <div class="relative mb-3">
                       <button class="absolute top-0 bottom-0 left-3 text-primary font-bold">
                         <FaSearch size={27} />
                       </button>
                       <input
-                        class="w-full bg-white text-secondary border border-gray-200 pl-10 py-2 md:py-[13px] px-4 leading-tight text-sm focus:outline-none focus:bg-white focus:border-gray-500"
+                        class="w-full bg-white text-secondary border border-gray-200  pl-10 py-2 md:py-[13px] px-4 leading-tight text-sm focus:outline-none focus:bg-white focus:border-gray-500"
                         id="search"
                         type="text"
                         placeholder="Find Personnels"
@@ -159,30 +159,36 @@ function Main() {
                 </div>
                 <div class="border-b-2 w-full" />
                 <div class="p-2">
-                  <div class="flex flex-row md:flex-col space-x-5 px-2 py-3">
+                  <div class="flex flex-row md:flex-col space-x-5 px-2 py-3 md:py-0 md:pt-3">
                     <img
                       class="w-20 md:w-40 rounded-full border border-gray-100 shadow-sm md:mx-auto "
                       src="https://randomuser.me/api/portraits/men/11.jpg"
                       alt="user image"
                     />
-                    <div class="flex justify-start flex-col py-0 md:py-7">
+                    <div class="flex justify-start flex-col py-0 md:pt-5">
                       <h1 class="text-[14px] text-secondary font-bold self-start leading-3">
                         Name
                       </h1>
-                      <p class="text-secondary self-start text-[16px] font-medium pt-0 mb-2">
+                      <p class="text-secondary self-start text-[16px] font-medium pt-0 md:mb-2">
                         First name
                       </p>
                       <h1 class="text-[14px] text-secondary font-bold self-start leading-3">
                         Telephone
                       </h1>
-                      <p class="text-secondary self-start text-[16px] font-medium">
+                      <p class="text-secondary self-start text-[16px] font-medium md:mb-2">
                         08977
                       </p>
-                      <h1 class="text-[14px] text-secondary font-bold self-start leading-3 hidden">
+                      <h1 class="text-[14px] text-secondary font-bold self-start leading-3 hidden md:block">
                         Birthday
                       </h1>
-                      <p class="text-secondary self-start text-[16px] font-medium hidden">
+                      <p class="text-secondary self-start text-[16px] font-medium hidden md:block md:mb-2">
                         18-02
+                      </p>
+                      <h1 class="text-[14px] text-secondary font-bold self-start leading-3 hidden md:block">
+                        Email
+                      </h1>
+                      <p class="text-secondary self-start text-[16px] font-medium hidden md:block md:mb-2">
+                        test@mail
                       </p>
                     </div>
                   </div>
